aaf767c23b5c92e4ce46409976f3920aefa5cfe6
luke <nerorevenge@gmail.com>
2016-03-18 21:50:49 -0400

    New way of approaching things - split trees into hashmaps of trees

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,17 +1,21 @@
 class SearchController < ApplicationController
   def search
     term = params[:term]
-    result =  Dictionary.scan(term)
+    result =  Dictionary[term[0]]
+                        .scan(term)
                         .shuffle
 			.take(20)
 
     if result.empty?
       puts "mispelled"
       distance = 0
+
+      # select key from Correct randomly
+      randomKey = Correct.keys.shuffle[0]
       lresult = []
       while (lresult.empty?)
         distance += 1
-	lresult = Correct.query(term,distance)
+	lresult = Correct[randomKey].query(term,distance)
       end
       lresult = lresult
                    .keys
--- config/initializers/trie.rb
+++ config/initializers/trie.rb
@@ -1,8 +1,33 @@
 require 'rambling-trie'
 require 'bk'
-require 'YAML'
 
-::Dictionary = YAML.load(File.read('trie.yaml'))
-::Correct = YAML.load(File.read('bk.yaml'))
+::Correct = {}
+::Dictionary = {}
+
+dictionary_file = "dictionary.txt"
+
+file = File.open(dictionary_file,'r')
+
+huge_array = []
+
+alphabets = ("a".."z").to_a
+
+while (line = file.gets)
+  line = line.split("\n")[0]
+  huge_array << line
+end
+
+arrag = huge_array.group_by {|i| i[0] }
+
+alphabets.map do |i| 
+ Dictionary[i] = Rambling::Trie.create
+ Correct[i] = BK::Tree.new
+
+ arrag[i].map do |x| 
+	 Dictionary[i] << x 
+	 Correct[i].add x
+ end
+end
+
 
 
